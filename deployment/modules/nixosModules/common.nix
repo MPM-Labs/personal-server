61890adfd016669d14a14eb850f98cfe1177fbdf
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-05-29 16:16:04 +0200

    feat(portfolio): configure portfolio site

--- deployment/modules/nixosModules/common.nix
+++ deployment/modules/nixosModules/common.nix
@@ -6,24 +6,19 @@
       inputs.agenix.nixosModules.default
       app-user
       ssh-root
+      inputs.portfolio.nixosModules.default
     ];
 
-    # Example config of docker container
-    virtualisation.oci-containers = {
-      backend = "podman";
-      containers.envtest = {
-        user = "999:999";
-        image = "mendhak/http-https-echo:40";
-        environmentFiles = [ "/run/agenix/.env" ]; # Secrets available at /run/agenix/.env
-        ports = [ "80:8080" "443:8443" ];
-        pull = "newer";
-      };
+    services.portfolio = {
+      enable = true;
+      environmentFiles = [ "/run/secrets/.env" ];
     };
 
-    systemd.services."podman-envtest" = {
-      after = [ "agenix.service" ]; # Wait until agenix is finished
-      wants = [ "agenix.service" ];
-      restartTriggers = [ config.age.secrets.".env".file ]; # This is required for the container to restart when a secret is changed or added
+    services.caddy ={
+      enable = true;
+      virtualHosts."jonas.baugerud.no".extraConfig = ''
+        reverse_proxy http://localhost:3000
+      '';
     };
 
     networking.firewall.allowedTCPPorts = [ 80 443 ];
